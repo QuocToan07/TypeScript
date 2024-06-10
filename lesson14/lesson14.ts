3dc3965d1e8d96138f5127538f62c8e020afe33f
DESKTOP-UP0DOPV\TQT <tranquoctoanmain@gmail.com>
2024-06-10 16:39:37 +0700

    lesson14

---
+++ lesson14/lesson14.ts
@@ -0,0 +1,14 @@
+//UNION - join
+
+//Mong muốn: bắt lỗi khi complier code
+function addNumOrStr(a: number | string, b: number | string) {
+  if (typeof a === "number" && typeof b === "number") {
+    return a + b;
+  } else if (typeof a === "string" && typeof b === "string") {
+    return a.concat(b);
+  }
+  throw new Error("Param must str or num");
+}
+
+//lỗi khi running
+console.log(addNumOrStr("Quốc", "Toản"));
