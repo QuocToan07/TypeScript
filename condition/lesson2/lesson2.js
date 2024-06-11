2b3419eb8a2d1e43a83bf7f665f23fd676afd552
DESKTOP-UP0DOPV\TQT <tranquoctoanmain@gmail.com>
2024-06-11 09:22:45 +0700

    condition-lesson2

---
+++ condition/lesson2/lesson2.js
@@ -0,0 +1,34 @@
+// SWITCH CASE
+var myAge = 25;
+/*
+switch (myAge) {
+  case 10: {
+    console.log("Nhi đồng");
+    break;
+  }
+  case 20: {
+    console.log("Thanh thiếu niên");
+    break;
+  }
+  default: {
+    console.log("Già");
+    break;
+  }
+}
+*/
+// Cách viết gộp
+switch (myAge) {
+    case 10: {
+        console.log("Nhi đồng");
+        break;
+    }
+    case 20:
+    case 25: {
+        console.log("Thanh thiếu niên");
+        break;
+    }
+    default: {
+        console.log("Già");
+        break;
+    }
+}
